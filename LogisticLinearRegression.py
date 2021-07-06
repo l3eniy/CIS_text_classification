07150a7956287174cced9dbe4cc40e7b8e8c0179
Sandra Wölfel <woelfels@gmx.de>
2021-07-06 13:35:53 +0200

    Created Python Code to Vectorize a complete sentence and train the data by tensorflow

--- LogisticLinearRegression.py
+++ LogisticLinearRegression.py
@@ -1,7 +1,17 @@
+#code for logistic regression
+#nur ein feature berücksichtigt über .txt datei eingelesen, accuracy liegt bei 40%
+#Keras implementiert
+#Score Ergebnisse mit Keras:training accuracy:0.0837
+#training accuracy:0.1324
+
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from keras.models import Sequential
+from keras import layers
+from keras.backend import clear_session
+
 
 filepath_dict = {'CIS_labeled': 'BigDataTemplate4.txt'
 }
@@ -35,9 +45,37 @@ X_train
 
 classifier = LogisticRegression()
 classifier.fit(X_train, y_train)
-score = classifier.score(X_test, y_test)
+score1 = classifier.score(X_train , y_train)
+score2 = classifier.score(X_test, y_test)
 
-print("Accuracy", score)
+print("Accuracy: Train", score1)
+print("Accuracy: Test", score2)
+
+
+input_dim = X_train.shape[1]
+model = Sequential()
+model.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+model.summary()
+
+history = model.fit(X_train, y_train,
+                    epochs=100,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10
+                    )
+
+clear_session()
+
+loss, accuracy = model.evaluate(X_train,y_train, verbose=False)
+print("training accuracy:{:.4f}".format(accuracy))
+
+loss1, accuracy2 = model.evaluate(X_test, y_test, verbose=False)
+print("training accuracy:{:.4f}".format(accuracy2))
+
 
 
 
