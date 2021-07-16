a307af90ab74afaffc7f5d0c57d96236d8ad4929
l3eniy <pt!gD%f6%^01>
2021-07-16 16:41:16 +0200

    polish

--- blackbox.py
+++ blackbox.py
@@ -6,30 +6,28 @@ from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
-from joblib import dump, load
 import pickle
-
-
 
 df = pd.read_csv("BigDataTemplate4.txt", names=['CIS-Req', 'label'], sep='\t')
 cis_requirements = df['CIS-Req'].values
 funktionale_requirements = df['label'].values
 
+# Wie viele CIS_Reqs sind den Funktionalen jeweils zugeordnet? :
+print("label\tcount")
+print(df['label'].value_counts())
+
+
 list_X = ["Heute ist das Wetter nicht gut", "Heute ist das Wetter gut", "Heute das Wetter ist nicht gut",
-           "Das Wetter ist heute gut", "Kopfschmerzen sind schlimm!", "Heute habe ich Kopfschmerzen wegen dem Wetter"]
-list_Y = [0, 1, 0, 1, "String geht auch als Label", "String geht auch als Label"] # 0 --> schlechtes Wetter, 1 --> gutes Wetter, 3 --> Kopfschmerzen
-
-
-
+          "Das Wetter ist heute gut", "Kopfschmerzen sind schlimm!", "Heute habe ich Kopfschmerzen wegen dem Wetter"]
+list_Y = [0, 1, 0, 1, "String geht auch als Label",
+          "String geht auch als Label"]  # 0 --> schlechtes Wetter, 1 --> gutes Wetter, 3 --> Kopfschmerzen
 
 # global verfügbare Objekte
 VECTORIZER = CountVectorizer()  # Muss global verfügbar sein
 CLASSIFIER = SVC()
-# classifier = LogisticRegression()
-# classifier = RandomForestClassifier()
-# classifier = GradientBoostingClassifier()
-
-
+# CLASSIFIER = LogisticRegression()
+# CLASSIFIER = RandomForestClassifier()
+# CLASSIFIER = GradientBoostingClassifier()
 
 
 def prepare_trainingsdata(cis_list, funktional_list):
@@ -45,6 +43,7 @@ def prepare_trainingsdata(cis_list, funktional_list):
     cis_requirement_vectorized = VECTORIZER.transform(cis_list)
     return cis_requirement_vectorized, funktional_list
 
+
 def predict_funktionale_Anforderung(neuer_y_string):
     """
     Funktion bestimmt das zugehörige Label. Sie verwendet den globalen Vectorizer und den übergebenen classifier.
@@ -57,7 +56,8 @@ def predict_funktionale_Anforderung(neuer_y_string):
     print(neuer_y_string + "\t\t-->\t\t" + str(predicted_label[0]))
     return predicted_label[0]
 
-def print_mapping(X,Y, filter):
+
+def print_mapping(X, Y, filter):
     '''
     Gibt das Mapping von X und Y in Konsole aus. Mit filter auf "no" wird alles ausgegeben
     :param X: CIS_Requirement
@@ -73,16 +73,25 @@ def print_mapping(X,Y, filter):
             print(X[i] + "\t\t-->\t\t" + str(Y[i]))
 
 
+def save_persistent_model(pickle_file_name):
+    with open(pickle_file_name, "wb") as file:
+        pickle.dump(CLASSIFIER, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def use_persistent_model(pickle_file_name):
+    with open(pickle_file_name, 'rb') as handle:
+        classifier_from_pickle = pickle.load(handle)
+    return classifier_from_pickle
+
+# CLASSIFIER = use_persistent_model("test1.pickle")
 
 X, Y = prepare_trainingsdata(cis_requirements, funktionale_requirements)
 CLASSIFIER.fit(X, Y)  # Hier wird das ausgewählte Modell mit den trainingsdaten trainiert
 
-print_mapping(cis_requirements, funktionale_requirements, 1)
+# print_mapping(cis_requirements, funktionale_requirements, 1)
 
 test_X = "Ensure foobar services are not enabled"
 predict_funktionale_Anforderung(test_X)
 
-
-# save Classifier persistent
-with open("test.pickle", "wb") as file:
-    pickle.dump(CLASSIFIER, file, protocol=pickle.HIGHEST_PROTOCOL)
+# save Classifier persistent as pickle file
+# save_persistent_model("test1.pickle")
